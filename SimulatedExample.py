b71a67b357ae4df2e42a9e9ccaf14632a93b2228
yaopoppysong <yao.song@duke.edu>
2017-05-01 10:19:53 -0400

    Update SimulatedExample.py

--- SimulatedExample.py
+++ SimulatedExample.py
@@ -1,1 +1,31 @@
+# Using Simulated Data Set
 
+# load package
+import numpy as np
+import numpy.random as npr
+from sklearn.preprocessing import scale
+
+# Set initial data set
+npr.seed(123)
+X = np.random.normal(0, 1, (100, 6))
+Y = np.random.normal(0, 1, (100, 2))
+
+# normalize the matrices X and Y
+X = scale(X)
+Y = scale(Y)
+
+# Separate the data set
+X_train = X[:70, :]
+X_test = X[70:, :]
+Y_train = Y[:70, :]
+Y_test = Y[70:, :]
+
+# Using Partial Least Square Prediction Function
+Y_pred = pls_prediction(X_train, Y_train, X_test, 3, 1e-06)
+np.sum((Y_test-Y_pred)**2) # PRESS
+
+# Using Partial Least Square Package in Python
+pls1 = PLSRegression(n_components = 3)
+pls1.fit(X_train, Y_train)
+Y_pred1 = pls1.predict(X_test)
+np.sum((Y_test-Y_pred1)**2) # PRESS
